dbd8404ba4855b3581e700b7ece82ab614b393f8
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 20:13:38 +0530

    Day 1: Code of time converter program

---
+++ Day-01/MinutesToSecond.java
@@ -0,0 +1,9 @@
+// Java Program to convert Minutes into Seconds
+
+public class MinutesToSecond{
+    public static void main(String[] args){
+        int min = 5;
+        int second = min * 60;
+        System.out.println("Second = " + second);
+    }
+}
