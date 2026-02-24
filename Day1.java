e562ddbbc9a2e17955915fb2ffe122c91ab4810e
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 19:00:06 +0530

    Day1 product code

--- Day1.java
+++ Day1.java
@@ -1,10 +1,23 @@
 // Write a Java program to find the sum of two numbers 10 and 5. ----------------------
 
-public class Day1{
+/* public class Day1{
     public static void main(String[] args){
         int a = 10;
         int b = 5;
         int sum = a + b;
         System.out.println("Sum is : " + sum);
+    }
+}
+*/
+
+
+// Write a Java program to find the product of two numbers 8 and 4. ----------------------
+
+ public class Day1{
+    public static void main(String[] args){
+        int a = 8;
+        int b = 4;
+        int prod = a + b;
+        System.out.println("Product is : " + prod);
     }
 }
