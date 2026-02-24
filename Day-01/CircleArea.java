bea1decddd833b2e8fef0cdae4bf16dc14fec4fd
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 19:49:33 +0530

    Day 1: Code of circle area program

---
+++ Day-01/CircleArea.java
@@ -0,0 +1,9 @@
+// Java Program to find Area of a Circle --------------------------------------
+
+public class CircleArea{
+    public static void main(String[] args){
+        int rad = 7;
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+    }
+}
