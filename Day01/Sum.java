18d627c3a8be1d92010fd71ce3af0f7449915d5d
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 19:12:57 +0530

    Day 1: Code of sum program

---
+++ Day01/Sum.java
@@ -0,0 +1,11 @@
+
+// Write a Java program to find the sum of two numbers 10 and 5. ----------------------
+
+ public class Day1{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("Sum is : " + sum);
+    }
+}
