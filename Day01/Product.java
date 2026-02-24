a5a4c110b6e4267d33cbb80100e6d26048ab467a
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 19:18:28 +0530

    Day 1: Code of product program

---
+++ Day01/Product.java
@@ -0,0 +1,10 @@
+// Write a Java program to find the product of two numbers 8 and 4. ----------------------
+
+ public class Product{
+    public static void main(String[] args){
+        int a = 8;
+        int b = 4;
+        int prod = a + b;
+        System.out.println("Product is : " + prod);
+    }
+}
