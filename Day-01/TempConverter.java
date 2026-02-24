9f4c6caf104ff99cd288c54ea041e36bca6b492b
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 19:59:54 +0530

    Day 1: Code of temperature converter

---
+++ Day-01/TempConverter.java
@@ -0,0 +1,9 @@
+// Java Program to convert Celsius to Fahrenheit -------------------------------
+
+public class TempConverter{
+    public static void main(String[] args){
+        int Celsius = 37;
+        float Fahrenheit = (Celsius * 9/5f) + 32;
+        System.out.println("Fahrenheit : " + Fahrenheit);
+    }
+}
