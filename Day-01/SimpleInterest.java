72fbfc08f301d76928cf3e035cf29780600d2d9a
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 19:44:04 +0530

    Day 1: Code of simple interest program

---
+++ Day-01/SimpleInterest.java
@@ -0,0 +1,11 @@
+// Java Program to Calculate Simple Interest using Variables -------------------------------
+
+public class SimpleInterest{
+    public static void main(String[] args){
+        int P = 1000;
+        int R = 5;
+        int T = 2;
+        int SI = (P*R*T)/100;
+        System.out.println(SI);
+    }
+}
