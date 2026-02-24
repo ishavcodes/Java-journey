2bfaf7687e902f6af22d616064f298ee21f0ef1a
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 20:10:42 +0530

    Day 1: Code of triangle area program

---
+++ Day-01/TriangleArea.java
@@ -0,0 +1,10 @@
+// Java Program to find Area of a Triangle (1/2 * b * h) ------------------------------
+
+public class TriangleArea{
+    public static void main(String[] args){
+        int b = 10;
+        int h = 5;
+        float area = 1/2f * b * h;
+        System.out.println("Area = " + area);
+    }
+}
