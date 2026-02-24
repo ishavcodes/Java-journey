1d42571a6009f7e1c187d652c5e422e2312c8e52
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 19:23:31 +0530

    Day 1: Code of product program

--- Day01/Product.java
+++ Day01/Product.java
@@ -4,7 +4,7 @@
     public static void main(String[] args){
         int a = 8;
         int b = 4;
-        int prod = a + b;
+        int prod = a * b;
         System.out.println("Product is : " + prod);
     }
 }
