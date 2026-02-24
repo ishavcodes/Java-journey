2eb676a691ae48f69fc9caf72b596d3aaf738fd1
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 20:03:46 +0530

    Day 1: Code of average program

---
+++ Day-01/Average.java
@@ -0,0 +1,11 @@
+// Java Program to find Average of three numbers ----------------------------
+
+public class Average{
+    public static void main(String[] args){
+        int n1 = 10;
+        int n2 = 20;
+        int n3 = 30;
+        float avg = (n1+n2+n3)/3f;
+        System.out.println("Average = " + avg);
+    }
+}
