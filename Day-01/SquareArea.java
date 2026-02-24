f0e458678c5b2a92f007f014d1c7e7493131790b
Isha Kumari <isha.v.codes@gmail.com>
2026-02-24 20:07:50 +0530

    Day 1: Code of square area & perimeter program

---
+++ Day-01/SquareArea.java
@@ -0,0 +1,11 @@
+// Java Program to find Area and Perimeter of a Square
+
+public class SquareArea{
+    public static void main(String[] args){
+        int side = 5;
+        int area = side * side;
+        int per = 4 * side;
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + per);
+    }
+}
