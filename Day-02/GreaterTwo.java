43cadd68e3c2bf0bfe86c2b99b1007146a092184
Isha Kumari <isha.v.codes@gmail.com>
2026-02-25 14:28:44 +0530

    Day 2: Code for greater of two numbers

---
+++ Day-02/GreaterTwo.java
@@ -0,0 +1,22 @@
+// Java Program to Check Which Number is Greater Using User Input -----------------------
+
+import java.util.*;
+
+public class GreaterTwo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int num2 = sc.nextInt();
+        if(num1 > num2){
+            System.out.println(num1 + " is greater than " + num2);
+        }
+        else if(num2 > num1){
+            System.out.println(num2 + " is greater than " + num1);
+        }
+        else{
+            System.out.println("Both are equal");
+        }
+    }
+}
