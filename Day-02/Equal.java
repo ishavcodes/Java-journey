5ee7077f4516bb39e52937031502124385a3178b
Isha Kumari <isha.v.codes@gmail.com>
2026-02-25 14:26:52 +0530

    Day 2: Code for greater of two numbers

---
+++ Day-02/Equal.java
@@ -0,0 +1,19 @@
+// Java Program to Check if Two Numbers are Equal --------------------------------
+
+import java.util.*;
+
+public class Equal{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int num2 = sc.nextInt();
+        if(num1 == num2){
+            System.out.println("Numbers are equal");
+        }
+        else{
+            System.out.println("Numbers are not equal");
+        }
+    }
+}
