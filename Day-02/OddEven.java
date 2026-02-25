d7a6744a01c922b908cc4f87516ca5c10b58ce89
Isha Kumari <isha.v.codes@gmail.com>
2026-02-25 14:35:26 +0530

    Day 2: Code for odd even check

---
+++ Day-02/OddEven.java
@@ -0,0 +1,17 @@
+// Java Program to Check Odd or Even Number Using User Input ------------------------------------------
+
+import java.util.*;
+
+public class OddEven{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        if(num % 2 == 0){
+            System.out.println(num + " is an even number.");
+        }
+        else{
+            System.out.println(num + " is an odd number.");
+        }
+    }
+}
