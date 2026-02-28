da1779f3951a8f35bc5f9040c3528967d9a4beec
Isha Kumari <isha.v.codes@gmail.com>
2026-02-28 16:03:46 +0530

    Day 3: Triangle validity check

---
+++ Day-04/Triangle.java
@@ -0,0 +1,21 @@
+// Java Program to check if a Triangle is valid based on angles -----------------------------------
+
+import java.util.*;
+
+public class Triangle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first angle : ");
+        int a1 = sc.nextInt();
+        System.out.print("Enter second angle : ");
+        int a2 = sc.nextInt();
+        System.out.print("Enter third angle : ");
+        int a3 = sc.nextInt();
+        if((a1+a2+a3) == 180){
+            System.out.println("Valid Triangle.");
+        }
+        else{
+            System.out.println("Not a valid triangle.");
+        }
+    }
+}
