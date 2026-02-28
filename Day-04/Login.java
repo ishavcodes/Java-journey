e35cf2d97f98ad8e725150c4b7fd88f29296dfa4
Isha Kumari <isha.v.codes@gmail.com>
2026-02-28 16:03:13 +0530

    Day 3: Dual authentication login

---
+++ Day-04/Login.java
@@ -0,0 +1,19 @@
+// Java Program for a simple Login System (Username & Password) ---------------------------------
+
+import java.util.*;
+
+public class Login{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Username : ");
+        int user = sc.nextInt();
+        System.out.print("Enter Password : ");
+        int pass = sc.nextInt();
+        if(user == 111 && pass == 222){
+            System.out.println("Login Success.");
+        }
+        else{
+            System.out.println("Wrong Password.");
+        }
+    }
+}
