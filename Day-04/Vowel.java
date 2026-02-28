899388bddff031c36d5a902648ee054d9223c6d4
Isha Kumari <isha.v.codes@gmail.com>
2026-02-28 16:02:39 +0530

    Day 3: Vowel check using OR

---
+++ Day-04/Vowel.java
@@ -0,0 +1,17 @@
+// Java Program to check if a character is a Vowel or Consonant -------------------------------------
+
+import java.util.*;
+
+public class Vowel{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character : ");
+        char ch = sc.next().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            System.out.println("It's vowel.");
+        }
+        else{
+            System.out.println("It's consonant.");
+        }
+    }
+}
