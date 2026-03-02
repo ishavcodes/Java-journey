f018361af5c54651c270cd0046a54bbe505c5ba9
Isha Kumari <isha.v.codes@gmail.com>
2026-03-02 14:53:24 +0530

    Day 6: code for Sum of N

---
+++ Day-06/SumN.java
@@ -0,0 +1,18 @@
+// code for Sum of 1 to N -------------------------
+
+import java.util.*;
+
+public class SumN{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while(i <= n){
+            sum = sum + i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
