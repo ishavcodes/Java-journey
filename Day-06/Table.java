a1c8db22e864f2686d4a533d0912f55eb68e8c1e
Isha Kumari <isha.v.codes@gmail.com>
2026-03-02 14:55:09 +0530

    Day 6: code for Table of N

---
+++ Day-06/Table.java
@@ -0,0 +1,16 @@
+// code for Multiplication Table -----------------------
+
+import java.util.*;
+
+public class Table{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        int i = 1;
+        while(i <= 10){
+            System.out.println(n +" * "+ i + " = " + (n * i) );
+            i++;
+        }
+    }
+}
