8ea69aaff02470f2ed27e750239986f35af8127a
Isha Kumari <isha.v.codes@gmail.com>
2026-03-03 21:24:45 +0530

    Day 7: Code for Factors of N

---
+++ Day-07/Factors.java
@@ -0,0 +1,16 @@
+// code for Factors of N using For Loop ------------------
+
+import java.util.*;
+
+public class Factors{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                System.out.println(i + " is a factor.");
+            }
+        }
+    }
+}
