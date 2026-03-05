4164931224a5d9fb47a8fdbc523702233cc3abc9
Isha Kumari <isha.v.codes@gmail.com>
2026-03-05 22:47:57 +0530

    Day 9: Code for Prime Number

---
+++ Day-09/Prime.java
@@ -0,0 +1,21 @@
+// code for Prime Number Check using For Loop ------------------
+
+import java.util.*;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        boolean isPrime = true;
+
+        if (n <= 1) isPrime = false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
