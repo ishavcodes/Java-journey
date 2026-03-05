6b66b2b9a70dfc2ad38a8fc407894d023b6bf6fa
Isha Kumari <isha.v.codes@gmail.com>
2026-03-05 22:49:57 +0530

    Day 9: Code for Print Odd

---
+++ Day-09/OnlyOdd.java
@@ -0,0 +1,12 @@
+// code for Print Odd Numbers using Continue ------------------
+
+import java.util.*;
+
+public class OnlyOdd {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) continue;
+            System.out.println(i);
+        }
+    }
+}
