5783002b6bcb09ab6a8194c9bc3d4878f84a4802
Isha Kumari <isha.v.codes@gmail.com>
2026-03-07 21:03:33 +0530

    Day 11: Inverted Pyramid

---
+++ Day-11/InvertedPyramid.java
@@ -0,0 +1,12 @@
+// code for Inverted Half Pyramid ------------------
+
+public class InvertedPyramid{
+    public static void main(String[] args) {
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
