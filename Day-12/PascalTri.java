5aebe3afe980c38bcdcbc52d91e04685829c06e0
Isha Kumari <isha.v.codes@gmail.com>
2026-03-08 11:54:17 +0530

    Day 12: Pascals Triangle

---
+++ Day-12/PascalTri.java
@@ -0,0 +1,18 @@
+// code for Pascal's Triangle ------------------
+
+public class PascalTri{
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
