d427705747d55dea1febb20992438e66db510619
Isha Kumari <isha.v.codes@gmail.com>
2026-03-08 11:55:22 +0530

    Day 12: X Pattern

---
+++ Day-12/CrossPattern.java
@@ -0,0 +1,18 @@
+// code for X Shape Pattern ------------------
+
+public class CrossPattern{
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j || i + j == n + 1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
