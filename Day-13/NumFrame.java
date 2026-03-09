b05000788ac8cd17bba7d50200e2abdcbbd9d30b
Isha Kumari <isha.v.codes@gmail.com>
2026-03-09 12:18:12 +0530

    Day 13: Code for Frame Logic

---
+++ Day-13/NumFrame.java
@@ -0,0 +1,14 @@
+// code for Square Frame Pattern ------------------
+
+public class NumFrame{
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) System.out.print("* ");
+                else System.out.print(i-1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
