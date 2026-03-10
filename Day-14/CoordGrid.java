35d971c76afab2671fc6fbf72863dc7badc77af3
Isha Kumari <isha.v.codes@gmail.com>
2026-03-10 15:15:47 +0530

    Day 14: Code for Coord Grid

---
+++ Day-14/CoordGrid.java
@@ -0,0 +1,17 @@
+// code for Coordinate Grid (i,j) ------------------
+
+import java.util.*;
+
+public class CoordGrid{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print("(" + i + "," + j + ")" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
