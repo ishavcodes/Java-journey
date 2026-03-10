cff332513288b58384aa9b872cc43c76b453a9c6
Isha Kumari <isha.v.codes@gmail.com>
2026-03-10 15:12:17 +0530

    Day 14: Code for Repeat Number

---
+++ Day-14/RepeatNum.java
@@ -0,0 +1,17 @@
+// code for Repeated Number Pattern ----------------------
+
+import java.util.*;
+
+public class RepeatNum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
