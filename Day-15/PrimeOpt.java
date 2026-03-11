3dffe911d2c53dbc630fae836e8f4d8e9f1334a0
Isha Kumari <isha.v.codes@gmail.com>
2026-03-11 16:38:34 +0530

    Day 15 : Code for Optimized Prime Check using Functions

---
+++ Day-15/PrimeOpt.java
@@ -0,0 +1,26 @@
+// code for Optimized Prime Check using Functions ------------------------
+
+import java.util.*;
+
+public class PrimeOpt{
+
+    public static boolean isPrime(int n){
+        if( n == 2){
+            return true;
+        }
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if( n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        boolean PrimeCheck = isPrime(n);
+        System.out.println(PrimeCheck);
+    }
+}
