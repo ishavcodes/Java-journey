ea35c4496dd753b80f81c1d31d99f8458dbd3b66
Isha Kumari <isha.v.codes@gmail.com>
2026-03-14 12:07:37 +0530

    Day 18 : BS on reverse sorted array

---
+++ Day-18/BinarySearchDesc.java
@@ -0,0 +1,49 @@
+// code for Binary Search on Descending Array ---------------------------
+
+import java.util.*;
+
+public class BinarySearchDesc{
+
+    public static int getIndex(int arr[], int key, int size){
+        int start = 0;
+        int end = size - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[mid] > key ){
+                start = mid + 1;
+            }
+            if(arr[mid] < key){
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter elements of array : ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter a key : ");
+        int key = sc.nextInt();
+
+        int ans = getIndex(arr, key, size);
+
+        if(ans == -1){
+            System.out.println("Not Found.");
+        }else{
+            System.out.println("Element found at index : " + ans);
+        }
+    }
+}
