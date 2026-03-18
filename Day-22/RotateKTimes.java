cb316d81cfcc96369a8a1b7e09ca4e50afe94e6b
Isha Kumari <isha.v.codes@gmail.com>
2026-03-18 19:59:04 +0530

    Day 22 : Code for multi-step array rotation

---
+++ Day-22/RotateKTimes.java
@@ -0,0 +1,47 @@
+// Rotate array to the right by K steps ---------------------------
+
+import java.util.*;
+
+public class RotateKTimes{
+
+    public static void reverse(int arr[], int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void rotate(int arr[], int k){
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter elements of array : ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter number of rotations(k) : ");
+        int k = sc.nextInt();
+
+        rotate(arr, k);
+
+        System.out.println("Array after rotation : " + Arrays.toString(arr));
+    }
+}
