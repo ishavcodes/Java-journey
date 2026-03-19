c6c39a015917983dab603ae0948b3904a9cd6fbe
Isha Kumari <isha.v.codes@gmail.com>
2026-03-19 13:46:31 +0530

    Day 23 : Code for single unique element search

---
+++ Day-23/UniqueElement.java
@@ -0,0 +1,24 @@
+// Find the only element that doesn't repeat ------------------
+
+import java.util.*;
+
+public class UniqueElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Size: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        for(int i=0; i<n; i++) {
+            int count = 0;
+            for(int j=0; j<n; j++) {
+                if(arr[i] == arr[j]) count++;
+            }
+            if(count == 1) {
+                System.out.println("Unique Element: " + arr[i]);
+                break;
+            }
+        }
+    }
+}
