9b6ce3108215e25d0d6d0f3a6d2ac8aa66e96a09
Isha Kumari <isha.v.codes@gmail.com>
2026-03-23 10:22:24 +0530

    Day 26 : Code for vowel and consonant counting

---
+++ Day-26/VowelConsonant.java
@@ -0,0 +1,20 @@
+// Count total vowels and consonants in a string ------------------------------------
+
+
+import java.util.*;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().toLowerCase();
+        int v = 0, c = 0;
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch >= 'a' && ch <= 'z') {
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') v++;
+                else c++;
+            }
+        }
+        System.out.println("Vowels: " + v + ", Consonants: " + c);
+    }
+}
