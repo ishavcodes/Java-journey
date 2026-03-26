6d061bbd43f0d3c2c09dcba3708d6435eb5311bc
Isha Kumari <isha.v.codes@gmail.com>
2026-03-26 12:21:02 +0530

    Day 29 : Code for spiral traversal of 2D array

---
+++ Day-29/SpiralPrint.java
@@ -0,0 +1,27 @@
+// Print matrix elements in a spiral clockwise order -----------------------
+
+import java.util.*;
+
+public class SpiralPrint {
+    public static void printSpiral(int[][] a, int r, int c) {
+        int top = 0, bottom = r-1, left = 0, right = c-1;
+        while(top <= bottom && left <= right) {
+            for(int i=left; i<=right; i++) System.out.print(a[top][i] + " ");
+            top++;
+            for(int i=top; i<=bottom; i++) System.out.print(a[i][right] + " ");
+            right--;
+            if(top <= bottom) {
+                for(int i=right; i>=left; i--) System.out.print(a[bottom][i] + " ");
+                bottom--;
+            }
+            if(left <= right) {
+                for(int i=bottom; i>=top; i--) System.out.print(a[i][left] + " ");
+                left++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[][] m = {{1,2,3},{4,5,6},{7,8,9}};
+        printSpiral(m, 3, 3);
+    }
+}
